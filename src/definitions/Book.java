0174ff365f8e0595d273863e30fc78f27699c9fe
komal-bindal <komal.bindal_cs19@gla.ac.in>
2020-08-23 23:38:40 +0530

    create getter and setter methods for the fields in Book.java class.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,28 @@ public class Book {
     private String name;
     private String authorName;
     private String bookISBNNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getBookISBNNumber() {
+        return bookISBNNumber;
+    }
+
+    public void setBookISBNNumber(String bookISBNNumber) {
+        this.bookISBNNumber = bookISBNNumber;
+    }
 }
