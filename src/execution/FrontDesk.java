2efccc65c4ca8acf6110f9eca1cc772117cc1dde
komal-bindal <komal.bindal_cs19@gla.ac.in>
2020-08-27 00:04:26 +0530

    create the introductory message for FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,19 +6,13 @@
  * */
 package execution;
 
-import definitions.Book;
-import definitions.Library;
-import definitions.Student;
-
 public class FrontDesk {
     public static void main(String[] args) {
-        Library lib = new Library();
-        lib.listBooks();
-
-        Student student = new Student();
-        System.out.println(student);
-
-        Book book = new Book();
-        System.out.println(book);
+        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+        System.out.println("How may I help you today?");
+        System.out.println("1. Issue a new book for me.");
+        System.out.println("2. Return a previously issues book from me.");
+        System.out.println("3. Show me all my issues books.");
+        System.out.println("4. Exit.");
     }
 }
