3c06bf95130971805bae8f918afc0a65c764d680
komal-bindal <komal.bindal_cs19@gla.ac.in>
2020-08-26 23:32:19 +0530

    create all utility components for Student.java class.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,5 +6,85 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Student {
+    int numberOfBooksIssued;
+    Book[] nameOfBooksIssued;
+    private String name;
+    private long universityRollNumber;
+
+    public Student() {
+        name = "Dev";
+        universityRollNumber = 191500200L;
+        numberOfBooksIssued = 0;
+        this.nameOfBooksIssued = new Book[1];
+        nameOfBooksIssued[0] = new Book();
+    }
+
+    public Student(String name, long universityRollNumber, int numberOfBooksIssued, Book[] nameOfBooksIssued) {
+        this.name = name;
+        this.universityRollNumber = universityRollNumber;
+        this.numberOfBooksIssued = numberOfBooksIssued;
+        this.nameOfBooksIssued = nameOfBooksIssued;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Book[] getNameOfBooksIssued() {
+        return nameOfBooksIssued.clone();
+    }
+
+    public void setNameOfBooksIssued(Book[] nameOfBooksIssued) {
+        this.nameOfBooksIssued = nameOfBooksIssued;
+    }
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "numberOfBooksIssued: " + getNumberOfBooksIssued() +
+                ", nameOfBooksIssued: " + Arrays.toString(getNameOfBooksIssued()) +
+                ", name: '" + getName() + '\'' +
+                ", universityRollNumber: " + getUniversityRollNumber();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return getNumberOfBooksIssued() == student.getNumberOfBooksIssued() &&
+                getUniversityRollNumber() == student.getUniversityRollNumber() &&
+                Arrays.equals(getNameOfBooksIssued(), student.getNameOfBooksIssued()) &&
+                Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getNumberOfBooksIssued(), getName(), getUniversityRollNumber());
+        result = 31 * result + Arrays.hashCode(getNameOfBooksIssued());
+        return result;
+    }
 }
